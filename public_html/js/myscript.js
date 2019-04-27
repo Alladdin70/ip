e97b42022b9a99abb9a9d2d3661b6c056764f04d
Sergey <repin.sergej@gmail.com>
2019-04-27 19:44:59 +0300

    datepicker presents

--- public_html/js/myscript.js
+++ public_html/js/myscript.js
@@ -13,29 +13,48 @@ $(document).ready(function(){
     $('.row').prepend('<div class = "col-md-4" id = "btn2"></div>');
     $('.row').prepend('<div class = "col-md-4" id = "btn3"></div>');
     $('#btn1').append('<button type="button" class="btn btn-success btn-md \n\
-btn-block">Block level button</button>');
+    btn-block">Block level button</button>');
     $('#btn2').append('<button type="button" class="btn btn-success btn-md \n\
-btn-block">Block level button</button>');    
+    btn-block">Block level button</button>');    
     $('#btn3').append('<button type="button" class="btn btn-success btn-md \n\
-btn-block">Block level button</button>');
+    btn-block">Block level button</button>');
     $('body').prepend('<div class = "titlebox"></div>');
     $('.titlebox').append('<div class = "title"></div>');
     $('.title').text("It 's the titlebox");
     $('body').append('<div class = "formbox"></div>');
-    $('.formbox').append('<button type="button" class="btn btn-success btn-md">\n\
-Block level button</button>');
+    $('.formbox').after('<div class = "underform"></div>');
+    $('.underform').append('<button type="button" class="btn btn-success btn-md">\n\
+    Block level button</button>');
+    $('.underform').hide();
     $('.formbox').hide();
-//    $('#btn3 button').prop("disabled",true)
-//    $('#btn1 button').prop("disabled",false)
-    $('#btn1 button').click(function(){
-            hideButtons();
-            showFormBox();
-    });
-    $('.formbox button').click(function(){
+    $('.underform button').click(function(){
             showButtons();
             hideFormBox();
+            
+    });  
+    $('#btn1 button, #btn2 button').click(function(){
+        hideButtons();
+        showFormBox();
+        $('.formbox').prepend('<p></p>');
+        $('p').append('<label for="from">с:</label> <input type="text" id="from" name="from"/>');
+        $('p').append('<label for="to">до:</label> <input type="text" id="to" name="to"/>');
+        var dates = $("#from, #to").datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 1,
+        onSelect: function(selectedDate){
+            var option = this.id == "from" ? "minDate" : "maxDate",
+            instance = $( this ).data( "datepicker" ),
+            date = $.datepicker.parseDate(
+            instance.settings.dateFormat || $.datepicker._defaults.dateFormat,
+            selectedDate, instance.settings);
+            dates.not(this).datepicker("option", option, date);
+            }
+        });            
     });
-});
+  
+    
+  });
 
 function hideButtons(){
     $('.row').hide();
@@ -47,8 +66,11 @@ function showButtons() {
 
 function showFormBox() {
     $('.formbox').show();
+    $('.underform').show();
 }
 
 function hideFormBox() {
+    $('.formbox').empty();
     $('.formbox').hide();
+    $('.underform').hide();
 }
